cc415f81f8badda1ed09d4d65970fbc32cb4de27
hirossan4049 <haruto405329@gmail.com>
2023-04-23 07:39:52 +0900

    db 休憩時間追加

---
+++ server/prisma/seed.ts
@@ -0,0 +1,15 @@
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+async function main() {
+  console.log('seeded');
+}
+
+main()
+  .catch((e) => {
+    console.error(e);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
