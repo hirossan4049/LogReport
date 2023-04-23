d4395582463e3a065275602353940780979e240a
hirossan4049 <haruto405329@gmail.com>
2023-04-23 14:29:56 +0900

    日付Switcher作成

---
+++ client/src/components/YearMonthSwitcher.tsx
@@ -0,0 +1,86 @@
+import { HStack, IconButton, Text, Tooltip } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
+import {
+  BsChevronDoubleLeft,
+  BsChevronLeft,
+  BsChevronRight,
+  BsChevronDoubleRight,
+} from "react-icons/bs";
+
+export const YearMonthSwitcher = (props: {
+  year: number;
+  month: number;
+  onChange: (year: number, month: number) => void;
+}) => {
+  const [year, setYear] = useState(props.year);
+  const [month, setMonth] = useState(props.month);
+
+  useEffect(() => {
+    props.onChange(year, month);
+  }, []);
+
+  const handlePrevMonth = () => {
+    if (month === 1) {
+      setYear(year - 1);
+      setMonth(12);
+    } else {
+      setMonth(month - 1);
+    }
+  };
+
+  const handleNextMonth = () => {
+    if (month === 12) {
+      setYear(year + 1);
+      setMonth(1);
+    } else {
+      setMonth(month + 1);
+    }
+  };
+
+  const handlePrevYear = () => {
+    setYear(year - 1);
+  };
+
+  const handleNextYear = () => {
+    setYear(year + 1);
+  };
+
+  return (
+    <HStack spacing={0}>
+      <Tooltip label={"1年前"}>
+        <IconButton
+          onClick={handlePrevYear}
+          bg={"clear"}
+          icon={<BsChevronDoubleLeft />}
+          aria-label={"1年前"}
+        />
+      </Tooltip>
+      <Tooltip label={"1ヶ月前"}>
+        <IconButton
+          onClick={handlePrevMonth}
+          bg={"clear"}
+          icon={<BsChevronLeft />}
+          aria-label={"1ヶ月前"}
+        />
+      </Tooltip>
+      <Text px={2}>{year}年</Text>
+      <Text px={2}>{month}月</Text>
+      <Tooltip label={"1ヶ月後"}>
+        <IconButton
+          onClick={handleNextMonth}
+          bg={"clear"}
+          icon={<BsChevronRight />}
+          aria-label={"1ヶ月後"}
+        />
+      </Tooltip>
+      <Tooltip label={"1年後"}>
+        <IconButton
+          onClick={handleNextYear}
+          bg={"clear"}
+          icon={<BsChevronDoubleRight />}
+          aria-label="1年後"
+        />
+      </Tooltip>
+    </HStack>
+  );
+};
--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,7 +1,20 @@
+import { VStack, Text, HStack, IconButton, Icon } from "@chakra-ui/react";
+import { YearMonthSwitcher } from "../components/YearMonthSwitcher";
+import { useState } from "react";
+
+
 export const Home = () => {
+  const [year, setYear] = useState(2023);
+  const [month, setMonth] = useState(1);
+
+  const handleYearMonthChange = (year: number, month: number) => {
+    setYear(year)
+    setMonth(month)
+  };
+
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <VStack>
+      <YearMonthSwitcher year={year} month={month} onChange={handleYearMonthChange} />
+    </VStack>
   );
 };
