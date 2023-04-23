cf35d7759f629ee84aaffe86ad8ba9cc32331b1e
hirossan4049 <haruto405329@gmail.com>
2023-04-23 12:18:34 +0900

    cors対応

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 import userController from './controllers/userController';
 import reportController from './controllers/reportController';
 import { Auth } from './libs/auth';
@@ -6,9 +7,10 @@ import { Auth } from './libs/auth';
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.use('/user', userController);
-app.use('/report', reportController)
+app.use('/report', reportController);
 
 app.get('/', Auth.verify, async (req, res) => {
   if (req.userId) {
