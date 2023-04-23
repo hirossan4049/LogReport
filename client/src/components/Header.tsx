a3e9c158f8e799bc22e6ca70f6ffb874362a90d3
hirossan4049 <haruto405329@gmail.com>
2023-04-23 12:43:27 +0900

    ログインしたらヘッダーをいい感じにする

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,17 +1,53 @@
-import { HStack, Text, Button, Divider, VStack, Link } from "@chakra-ui/react";
+import {
+  HStack,
+  Text,
+  Button,
+  Divider,
+  VStack,
+  Link,
+  Avatar,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList,
+  MenuDivider,
+} from "@chakra-ui/react";
+import { useCookies } from "react-cookie";
 
 export const Header = () => {
-  return (
-    <VStack w={"full"}>
-      <HStack w={"full"} p={2} px={8}>
-        <Text w="full">LogReport</Text>
-        <Link href="/login">
-          <Button>ログイン</Button>
-        </Link>
+  const [cookies, _, removeCookie] = useCookies(["token"]);
 
-        <Link href="/register">
-          <Button>新規登録</Button>
-        </Link>
+  const handleLogout = () => {
+    removeCookie("token");
+  };
+
+  return (
+    <VStack w={"full"} spacing={0}>
+      <HStack w={"full"} p={2} h={14}>
+        <Text w="full" px={8}>LogReport</Text>
+        {cookies.token ? (
+          <Menu>
+            <MenuButton>
+              <Avatar name={"user name"} h={12} w={12} />
+            </MenuButton>
+            <MenuList>
+              <MenuItem>プロフィール</MenuItem>
+              <MenuItem>設定</MenuItem>
+              <MenuDivider />
+              <MenuItem color={"red"} onClick={handleLogout}>ログアウト</MenuItem>
+            </MenuList>
+          </Menu>
+        ) : (
+          <>
+            <Link href="/login">
+              <Button>ログイン</Button>
+            </Link>
+
+            <Link href="/register">
+              <Button>新規登録</Button>
+            </Link>
+          </>
+        )}
       </HStack>
       <Divider />
     </VStack>
