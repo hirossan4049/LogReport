39959daf8cd576911b95bc08d8f4dca91578f8ef
hirossan4049 <haruto405329@gmail.com>
2023-04-27 17:14:31 +0900

    autocomplete微調整

---
+++ server/python/autocomplete.py
@@ -0,0 +1,49 @@
+import os
+import sys
+from datetime import datetime, timezone
+import time
+from github import Github
+import openai
+
+repository = sys.argv[1]
+username = sys.argv[2]
+start_time = time.mktime(datetime.fromisoformat(
+    sys.argv[3].replace('Z', '+00:00')).timetuple())
+end_time = time.mktime(datetime.fromisoformat(
+    sys.argv[4].replace('Z', '+00:00')).timetuple())
+
+github_token = os.environ.get("GITHUB_API_TOKEN")
+
+g = Github(github_token)
+repo = g.get_repo(repository)
+commits = repo.get_commits(author=username)
+comments_list = []
+for commit in commits:
+    tdate = time.mktime(commit.commit.committer.date.timetuple())
+    if start_time <= tdate <= end_time:
+        comments_list.append(commit.commit.message)
+
+openai.api_key = os.environ.get("OPENAI_API_KEY")
+engine = os.environ.get("COMPLETE_ENGINE") or "text-davinci-002"
+
+if engine == "text-davinci-002":
+    response = openai.Completion.create(
+        engine="text-davinci-002",
+        prompt=f"これはgitlogです。各要素を結合して、全体を15文字でまとめてください：{comments_list}",
+        max_tokens=20,
+        n=1,
+        stop=None,
+    )
+    print(response.choices[0].text.strip())
+elif engine == "gpt-3.5-turbo":
+    res = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": "以下はgitlogのメッセージです。要約したメッセージのみ出力せよ"},
+            {"role": "system", "content": "15字以内にまとめよ"},
+            {"role": "user", "content": f"{comments_list}"}
+        ]
+    )
+    print(res.choices[0].message.content)
+else:
+    raise "engine is not supported"
--- server/src/libs/getReport.ts
+++ server/src/libs/getReport.ts
@@ -4,8 +4,13 @@ import util from 'util';
 const exec = util.promisify(childProcess.exec);
 
 export const getReport = async (owner: string, repoName: string, from: Date, to: Date, author: string) => {
+  const github_api_key = process.env.GITHUB_API_TOKEN;
+  const chatgpt_api_key = process.env.CHATGPT_API_TOKEN;
   // FIXME: hack
-  //   const message = await exec(`python3 ${owner} ${repoName} ${from} ${to} ${author}`);
-  await new Promise(resolve => setTimeout(resolve, 1000)) 
-  return 'バグ修正';
+  // const message = await exec(`python3 ${owner} ${repoName} ${from.toISOString()} ${to.toISOString()} ${author}`);
+  const message = await exec(
+    `GITHUB_API_TOKEN=${github_api_key} OPENAI_API_KEY=${chatgpt_api_key} python3 python/autocomplete.py ${owner} ${repoName} ${from.toISOString()} ${to.toISOString()} ${author}`
+  );
+  // await new Promise(resolve => setTimeout(resolve, 1000))
+  return message.stdout;
 };
